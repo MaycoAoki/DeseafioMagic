da0ef674a7921c1c8d2d920524e2b095f1ad1907
Mayco Aoki <maycoaoki@gmail.com>
2020-06-10 21:44:52 -0400

    Add fundo da home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,8 @@
 @section('content')
   <div class="content">
     <div class="container-fluid">
-      
+      <h1 class="display-3">Biblioteca de Filme</h1>
+      <div class="page-header login-page header-filter" filter-color="black" style="background-image: url('{{ asset('material') }}/img/banner.jpg'); background-size: cover; background-position: top center;align-items: center;" data-color="purple">
     </div>
   </div>
 @endsection
